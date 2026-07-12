b8aa93c95d7b9662268bf83787e203b17e3c889d
Bryson414 <brwnc2022@gmail.com>
2026-07-12 00:58:38 -0400

    Update sidebar.jsx

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,9 +1,1 @@
-function SummaryCard({ title, value }) {
-  return <div className="summaryCard"><h3>{title}:</h3><p>{value}</p></div>
-}
 
-<SummaryCard title="Unread Emails" value="18" />
-
-<SummaryCard title="Needs Reply" value="5" />
-
-<SummaryCard title="Bills" value="2" />
